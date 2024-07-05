871abb20f7e9dde80157a69fce43371ac2d9b64d
Fathima <nikhath.fathima@capgemini.com>
2024-07-05 21:28:18 +0530

    pushing my entire java basic code

---
+++ Initialization_Declaration.java
@@ -0,0 +1,11 @@
+package java__basics_prgms;
+
+public class Initialization_Declaration {
+
+	public static void main(String[] args) {
+		int a=20;//Initialization_Declaration
+		a=90;//updating
+		System.out.println(a);
+	}
+
+}
---
+++ Printing_Your_Name.java
@@ -0,0 +1,10 @@
+package java__basics_prgms;
+
+public class Printing_Your_Name {
+
+	public static void main(String[] args) {
+		String a="Nikhath Fathima";
+		System.out.println(a);
+	}
+
+}
